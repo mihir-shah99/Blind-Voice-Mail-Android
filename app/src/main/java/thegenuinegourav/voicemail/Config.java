19e928f2282623832ae9529ae28da22b6dbd801a
Mihir Shah <mihir.shah8260@gmail.com>
2021-06-02 16:16:10 +0530

    Removed Redundant code chunk

---
+++ app/src/main/java/thegenuinegourav/voicemail/Config.java
@@ -0,0 +1,6 @@
+package thegenuinegourav.voicemail;
+
+public class Config {
+    public static String EMAIL ="your-gmail-address";  //gmail address
+    public static String PASSWORD ="your-password"; //password
+}
